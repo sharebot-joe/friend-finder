decdb038826e2de1c878dbabc55712ec2bcaa2b8
Joseph Kim <josephikim2@gmail.com>
2018-05-23 00:48:43 -0700

    base html

---
+++ server.js
@@ -0,0 +1,145 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+// Tables (DATA)
+// =============================================================
+// Comprised of objects with the following form:
+// {
+//     customerName: "",
+//     phoneNumber: "",
+//     customerEmail: "",
+//     customerID: ""
+//  }
+var tables = [];
+// Waitlist (DATA)
+// =============================================================
+// Comprised of objects with the following form:
+// {
+//     customerName: "",
+//     phoneNumber: "",
+//     customerEmail: "",
+//     customerID: ""
+//  }
+var waitlist = [];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// API tables page which displays tables JSON data
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+  // res.sendFile(path.join(__dirname, "view.html"));
+});
+
+// API waitlist page which displays waitlist JSON data
+app.get("/api/waitlist", function(req, res) {
+  return res.json(waitlist);
+  // res.sendFile(path.join(__dirname, "view.html"));
+});
+
+// Displays current reservatiosn and waiting list
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+// Displays a single character, or returns false
+app.get("/reservations", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservations.html"));
+  // var chosen = req.params.character;
+
+  // console.log(chosen);
+
+  // for (var i = 0; i < characters.length; i++) {
+  //   if (chosen === characters[i].routeName) {
+  //     return res.json(characters[i]);
+  //   }
+  // }
+
+  // return res.json(false);
+});
+
+// app.post("/api/characters", function(req, res) {
+//   // req.body hosts is equal to the JSON post sent from the user
+//   // This works because of our body-parser middleware
+//   var newcharacter = req.body;
+
+//   console.log(newcharacter);
+
+//   // We then add the json the user sent to the character array
+//   characters.push(newcharacter);
+
+//   // We then display the JSON to the users
+//   res.json(newcharacter);
+// });
+
+// Serve up static files to the client
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+
+function handleRequest(req, res) {
+  var path = req.url;
+
+  switch (path) {
+  case "/tables":
+    return renderTablesPage(req, res);
+  case "/waitlist":
+    return renderWaitlistPage(req, res);
+  default:
+    return renderHomePage(req, res);
+  }
+}
+
+function renderTablesPage(req, res) {
+  fs.readFile(__dirname + "/tables.html", function(err, data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+    }
+    else {
+      // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+      // an html file.
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+}
+
+function renderWaitlistPage(req, res) {
+  fs.readFile(__dirname + "/waitlist.html", function(err, data) {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/html" });
+      res.end("<html><head><title>Oops</title></head><body><h1>Oops, there was an error</h1></html>");
+    }
+    else {
+      // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
+      // an html file.
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    }
+  });
+}
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
