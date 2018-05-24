e2b1eb791ecd49694c3e9fd380c4237565768c35
Joseph Kim <josephikim2@gmail.com>
2018-05-23 20:20:57 -0700

    fixed .gitignore file

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var waitlist = [];
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "home.html"));
 });
 
 // API tables page which displays tables JSON data
