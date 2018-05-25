ccaa02dab901c1985af54c8318547eb30601cec2
Joseph Kim <josephikim2@gmail.com>
2018-05-24 23:50:56 -0700

    friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,34 @@
 // This file saves profile data as an array of objects. It follows the fomat below:
 // {
 //   "name":"Ahmed",
-//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "photo":"https://myphoto.jpg",
 //   "scores":[5,1,4,4,5,1,2,5,4,1]
 // }
+var characters = [
+  {
+    "name":"Apu",
+    "photo":"https://myphoto.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    "name":"Ahmed",
+    "photo":"https://myphoto.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    "name":"Ahmed",
+    "photo":"https://myphoto.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    "name":"Ahmed",
+    "photo":"https://myphoto.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  },
+  {
+    "name":"Ahmed",
+    "photo":"https://myphoto.jpg",
+    "scores":[5,1,4,4,5,1,2,5,4,1]
+  }
+]
+
